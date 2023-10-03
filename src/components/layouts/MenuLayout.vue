f424d9de151e3afec0686dbfa9cee042a07f95b5
Lilian <chci.lilian@gmail.com>
2023-10-03 16:49:19 +0200

    style : Layout of the menu
    Created a grid layout for the menu

---
+++ src/components/layouts/MenuLayout.vue
@@ -0,0 +1,58 @@
+<script setup>
+import MenuCard from '@/components/MenuCard.vue'
+</script>
+
+<template>
+  <div class="grid">
+    <MenuCard
+      title="Gyro Sandwich"
+      rating="4.9"
+      price="15.00"
+      imgSrc="/img/gyro.jpg"
+      imgAlt="Image of a gyro sandwich"
+    ></MenuCard>
+    <MenuCard
+      title="Enchilade"
+      rating="5.0"
+      price="22.50"
+      imgSrc="/img/enchilade.jpg"
+      imgAlt="Image of an enchilade"
+    ></MenuCard>
+    <MenuCard
+      title="Green Beans"
+      rating="4.9"
+      price="12.00"
+      imgSrc="/img/greenbeans.jpg"
+      imgAlt="Image of a green beans"
+    ></MenuCard>
+    <MenuCard
+      title="Pizza"
+      rating="5.0"
+      price="18.50"
+      imgSrc="/img/pizza.jpg"
+      imgAlt="Image of a pizza"
+    ></MenuCard>
+    <MenuCard
+      title="Chicken Pot Pie"
+      rating="4.9"
+      price="25.00"
+      imgSrc="/img/chickenpot.jpg"
+      imgAlt="Image of a chicken pot pie"
+    ></MenuCard>
+    <MenuCard
+      title="Green Salad"
+      rating="4.9"
+      price="15.00"
+      imgSrc="/img/salad.jpg"
+      imgAlt="Image of a salad"
+    ></MenuCard>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 40px;
+}
+</style>
