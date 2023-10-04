972d3b7949cc6b622f7e31c14906b7583d88030f
Lilian <chci.lilian@gmail.com>
2023-10-04 09:17:25 +0200

    eslint : New Rules

---
+++ src/components/elements/MyMY.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facere optio accusamus eos
+      repellendus neque obcaecati sunt ab quasi, laborum magni a exercitationem fugit deserunt
+      dolores dolore? Consectetur, consequatur voluptas.
+    </h1>
+  </div>
+</template>
